2821c9e1179159be20cb87e844aabe336634ce24
timislearning <tim.mitzen@gmail.com>
2020-10-20 11:52:28 -0400

    added readme

--- Cpu_turn.py
+++ Cpu_turn.py
@@ -2,14 +2,15 @@ from Scoreboard import ScoreBoard
 import random
 from print_scoreboard import print_scoreboard
 
-def cpu_scorings(cpu):#if the user punts and then cpu scores
+
+def cpu_scorings(cpu):  # if the user punts and then cpu scores
     if cpu == 1:
-        if random.random() <= .30:# 30 percent chance
+        if random.random() <= .30:  # 30 percent chance
             print("Cpu ran for 40 yards")
             print("\n\nForth down, Cpu kicks and scored a Field goal")
-            ScoreBoard.cpu_score.append(3) #adds to the cpu score
+            ScoreBoard.cpu_score.append(3)  # adds to the cpu score
             print_scoreboard()
-        elif random.random() <= .50:#fifty percent chance
+        elif random.random() <= .50:  # fifty percent chance
             print('\n\nCpu runs for a td!')
             ScoreBoard.cpu_score.append(7)
             print_scoreboard()
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Baseball game
+https://github.com/TimMitzen/textfootballgame.git
+
+open terminal and cd into whatever folder you cloned above
+
+type whatever python version plus baseball.py - python3 football.py
+
+follow instructions on the screen, p is for a pass, r is for for running the ball, s to show the scoreboard and q for quit.
+
+Game goes on for 2 minutes 
+
+# Todo
+
+Clean up the code
+
+Have quarters instead of being timed
